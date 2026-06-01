2c9e2afaf59f582541cef13c08f77f57b24b2f65
muminkuziev <muminkuziev@gmail.com>
2026-06-02 00:10:19 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,4 +51,4 @@ def run_flask():
 
 if __name__ == "__main__":
     threading.Thread(target=run_flask).start()
-    bot.infinity_polling(skip_pending=True
+    bot.infinity_polling(skip_pending=True)
