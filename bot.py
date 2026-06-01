d354eb858da8b13a7343b1d24133e0e180629a20
muminkuziev <muminkuziev@gmail.com>
2026-06-01 20:49:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
 import os
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Updater, CommandHandler
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
+def start(update, context):
+    update.message.reply_text(
         "Assalomu alaykum!\n\nIslamTimeWorldBot ga xush kelibsiz."
     )
 
-def main():
-    app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
 
-if __name__ == "__main__":
-    main()
+dp.add_handler(CommandHandler("start", start))
+
+updater.start_polling()
+updater.idle()
