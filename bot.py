652a4ee0892463446e345c4ac9a175dcd66d95d2
muminkuziev <muminkuziev@gmail.com>
2026-06-05 08:33:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -591,7 +591,7 @@ def location_handler(message):
 
             location_name = get_location_name(lat, lon)
 
-                if not location_name or location_name == "Siz yuborgan lokatsiya":
+                        if not location_name or location_name == "Siz yuborgan lokatsiya":
                 location_name = f"{lat:.4f}, {lon:.4f}"
 
 timezone_name = data["data"]["meta"]["timezone"]
